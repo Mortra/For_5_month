d4c4cd27dbb6fab55aff8874b93df6b2716a8a3b
Mortra <tttag@inbox.ru>
2022-09-25 21:07:01 +0600

    django_rest

--- movie_app/tests.py
+++ movie_app/tests.py
@@ -4,5 +4,5 @@ from django.test import TestCase
 
 # git remote add django_rest git@github.com:Mortra/For_5_month.git выбор репазиторий
 # git branch -M django
-# git push -u django_rest django django это branch, django_rest это origin
+# git push -u django_rest django - django это branch, django_rest это origin
 # git commit -m django_rest пушит проекта
